d43b651baf78aa2bea328415d6cc7ec0d7bf3c1f
demjanp <peter.demjan@gmail.com>
2024-05-05 12:48:26 +0200

    updated documentation

--- setup.py
+++ setup.py
@@ -61,6 +61,8 @@ setup(
 		'tqdm>=4.66.0, <5',
 		'requests>=2.31.0, <3',
 		'networkx>=3.3.0, <4',
+		'sphinx>=7.3.7, <7.4',
+		'myst-parser>=3.0.1, <3.1',
 	],
 	cmdclass={'bdist_wheel': bdist_wheel},
 )

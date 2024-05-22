207be969b17c4902ce0b509239e8331bae6f68bb
demjanp <peter.demjan@gmail.com>
2024-05-22 15:06:34 +0200

    added Phase class; updated visualization script

--- src/sitesyncro/__init__.py
+++ src/sitesyncro/__init__.py
@@ -1,7 +1,7 @@
 from sitesyncro.Model import Model
 from sitesyncro.Sample import Sample
 
-version_info = (0, 9, 5)
+version_info = (0, 9, 6)
 
 __version__ = '.'.join(map(str, version_info))
 __title__ = 'SiteSyncro'

306f09527613ac467b2e3ab9ffa89a2105988f38
demjanp <peter.demjan@gmail.com>
2024-05-22 15:15:17 +0200

    updated documentation

--- src/sitesyncro/__init__.py
+++ src/sitesyncro/__init__.py
@@ -1,8 +1,9 @@
 from sitesyncro.Model import Model
 from sitesyncro.Sample import Sample
+from sitesyncro.Phase import Phase
 
-version_info = (0, 9, 6)
+version_info = (0, 9, 7)
 
 __version__ = '.'.join(map(str, version_info))
 __title__ = 'SiteSyncro'
-__date__ = "15.5.2024"
+__date__ = "22.5.2024"
